4f5be31f637cdc556013e7fed642ca925e3600b0
kooooichi24 <ex.kouichi1224@gmail.com>
2025-06-14 16:47:45 +0900

    feat: Create spans at `rollTheDice()`

--- src/dice.ts
+++ src/dice.ts
@@ -8,9 +8,14 @@ function rollOnce(min: number, max: number) {
 }
 
 export function rollTheDice(rolls: number, min: number, max: number) {
-  const result: number[] = [];
-  for (let i = 0; i < rolls; i++) {
-    result.push(rollOnce(min, max));
-  }
-  return result;
+  // Create a span. A span must be closed.
+  return tracer.startActiveSpan('rollTheDice', (span: Span) => {
+    const result: number[] = [];
+    for (let i = 0; i < rolls; i++) {
+      result.push(rollOnce(min, max));
+    }
+    // Be sure to end the span!
+    span.end();
+    return result;
+  });
 }
